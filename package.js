35106484c2e3e69d8f63eb7ccedc31a7fa2dc19b
Fabian Kromer <fabian.kromer@gmail.com>
2025-07-28 17:33:11 +0000

    update version tag

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Support anonymous logins',
-  version: '0.5.4',
+  version: '0.5.5',
   name: 'faburem:accounts-anonymous',
   git: 'https://github.com/faburem/meteor-accounts-anonymous.git',
 })
